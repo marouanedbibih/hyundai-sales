042adb1a0158d0718a5bfae03881e410610f16bf
Marouane Dbibih <marrouane.dbibih@gmail.com>
2025-02-14 14:49:57 +0100

    After exam

--- backend/src/test/java/org/hyundai/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/hyundai/backend/BackendApplicationTests.java
@@ -1,13 +1,8 @@
 package org.hyundai.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
---
+++ docker/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS hyundai_sales_db;
+
+CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
+
+GRANT ALL PRIVILEGES ON hyundai_sales_db.* TO 'user'@'%';
+
+FLUSH PRIVILEGES;
