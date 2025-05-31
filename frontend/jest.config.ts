fc93eb2fff597cfc1c9c95c438ac59d0a16cd80d
Marouane Dbibih <marouane.dbibih@outlook.com>
2025-05-31 15:18:05 +0100

    Add the jest coverage configuration the jest.config.ts

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -10,8 +10,11 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+  testResultsProcessor: 'jest-sonar-reporter',
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
